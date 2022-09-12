bf30e40d25b959306957e8a683cbf1647d41a8bd
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2022-09-12 08:03:52 +0300

    loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,9 @@
 function Loading() {
 
     return(
-        <div>
-            <button type="button" className="bg-indigo-500 ..." disabled>
-                <svg className="motion-reduce:hidden animate-spin ..." viewBox="0 0 24 24"></svg>
+        <div className="text-color white py-3 px-3">
+            <button type="button" className="bg-indigo-500 border border-gray-700 rounded-lg px-3 py-3" disabled>
+                <svg className="motion-reduce:hidden animate-spin h-5 w-5 mr-3 py-3 px-3" viewBox="0 0 24 24"></svg>
                 Processing...
             </button>
         </div>
